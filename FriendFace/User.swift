20cecb1b769ac352d31911f6bcbcfa3ee3182513
GreenRamSilgen <kiranshrs@yahoo.com>
2025-05-24 21:56:33 -0700

    Created User struct representation

---
+++ FriendFace/User.swift
@@ -0,0 +1,50 @@
+//
+//  User.swift
+//  FriendFace
+//
+//  Created by Kiran Shrestha on 5/24/25.
+//
+
+import Foundation
+
+struct User : Codable, Hashable {
+    enum CodingKeys: CodingKey {
+        case id
+        case isActive
+        case name
+        case age
+        case company
+        case email
+        case address
+        case about
+        case registered
+        case tags
+        case friends
+    }
+    
+    var id: String
+    var isActive: Bool
+    var name: String
+    var age: Int
+    var company: String
+    var email: String
+    var address: String
+    var about: String
+    var registered: Date
+    var tags: [String]
+    var friends: [Friend]
+    
+    init(id: String, isActive: Bool, name: String, age: Int, company: String, email: String, address: String, about: String, registered: Date, tags: [String], friends: [Friend]) {
+        self.id = id
+        self.isActive = isActive
+        self.name = name
+        self.age = age
+        self.company = company
+        self.email = email
+        self.address = address
+        self.about = about
+        self.registered = registered
+        self.tags = tags
+        self.friends = friends
+    }
+}
