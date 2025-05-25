76acf9ca44971e35068e194c6894a4c6868a7ebd
GreenRamSilgen <kiranshrs@yahoo.com>
2025-05-24 21:58:58 -0700

    Added manual api call via sync button in toolbar. Added display to show data once recieved and allow for navigation as needed.

--- FriendFace/ContentView.swift
+++ FriendFace/ContentView.swift
@@ -8,14 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var users : [User] = []
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(users, id: \.id) { user in
+                    NavigationLink("\(user.name)", value: user)
+                }
+            }
+            .navigationDestination(for: User.self) { selection in
+                UserDetailView(user: selection)
+            }
+            .navigationTitle("Users")
+            .toolbar {
+                Button("Sync Data", systemImage: "arrow.trianglehead.2.clockwise.rotate.90"){
+                    Task {
+                        do {
+                            let apiUrl = URL(string: "https://www.hackingwithswift.com/samples/friendface.json")!
+                            let (data,_) = try await URLSession.shared.data(for: URLRequest(url: apiUrl))
+                            
+                            let decoder = JSONDecoder()
+                            decoder.dateDecodingStrategy = .iso8601
+                            
+                            let decodedData = try decoder.decode([User].self, from: data)
+                            
+                            users = decodedData
+                        }
+                        catch {
+                            print(error.localizedDescription)
+                        }
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
